8dd82abb5598ca558911eea8447ca4acaf7893b3
Chris Cromer <chris@cromer.cl>
2022-12-10 18:17:32 -0300

    make the word action link to the action page of the manual

--- src/models/action.h
+++ src/models/action.h
@@ -14,13 +14,13 @@ namespace obelisk
     {
         private:
             /**
-             * @brief The ID of the action in the knowledge base.
+             * @brief The ID of the Action in the knowledge base.
              *
              */
             int id_;
 
             /**
-             * @brief The name of the action.
+             * @brief The name of the Action.
              *
              */
             std::string name_;
@@ -39,7 +39,7 @@ namespace obelisk
             /**
              * @brief Construct a new Action object.
              *
-             * @param[in] id The ID of the action.
+             * @param[in] id The ID of the Action.
              */
             Action(int id) :
                 id_(id),
@@ -50,7 +50,7 @@ namespace obelisk
             /**
              * @brief Construct a new Action object.
              *
-             * @param[in] name The name of the action.
+             * @param[in] name The name of the Action.
              */
             Action(std::string name) :
                 id_(0),
@@ -61,8 +61,8 @@ namespace obelisk
             /**
              * @brief Construct a new Action object.
              *
-             * @param[in] id The ID of the action.
-             * @param[in] name The name of the action.
+             * @param[in] id The ID of the Action.
+             * @param[in] name The name of the Action.
              */
             Action(int id, std::string name) :
                 id_(id),
@@ -71,37 +71,37 @@ namespace obelisk
             }
 
             /**
-             * @brief Create the action table in the knowledge base.
+             * @brief Create the Action table in the knowledge base.
              *
              * @return const char* Returns the query used to create the table.
              */
             static const char* createTable();
 
             /**
-             * @brief Get the ID of the action.
+             * @brief Get the ID of the Action.
              *
              * @return int& Returns the ID.
              */
             int& getId();
 
             /**
-             * @brief Set the ID of the action.
+             * @brief Set the ID of the Action.
              *
-             * @param[in] id Set the ID of the action.
+             * @param[in] id Set the ID of the Action.
              */
             void setId(int id);
 
             /**
-             * @brief Get the name of the action.
+             * @brief Get the name of the Action.
              *
-             * @return std::string& The action name.
+             * @return std::string& The Action name.
              */
             std::string& getName();
 
             /**
-             * @brief Set the name of the action.
+             * @brief Set the name of the Action.
              *
-             * @param[in] name The name of the action.
+             * @param[in] name The name of the Action.
              */
             void setName(std::string name);
     };
